9b72f1a1e5398fbb133db3467480ade4efc7d78b
kitk4t3 <catalinadiaz30102009@gmail.com>
2025-07-21 18:57:02 +0000

    Actividad 1

--- MAIN.py
+++ MAIN.py
@@ -1,3 +1,12 @@
-b = 97
-a = 4
-print(a + b)
+# Historia 2
+# La serie sigue la historia de [Johnny] [Lawrence], exestudiante del dojo [Cobra Kai],
+# quien busca la redención reabriendo el infame dojo y reavivando, en el proceso,
+# su rivalidad con un ahora exitoso Daniel [LaRusso].
+# Este enemigo ha estado luchando por mantener el equilibrio en su vida sin la guía de su mentor, el señor [Miyagi].
+# La serie trata sobre dos hombres que enfrentan sus demonios del pasado y
+# resuelven las frustraciones del presente de la única manera en que saben hacerlo: a través del karate.
+=input
+=input
+=input
+=input
+=input
