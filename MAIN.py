4f00fe29b87e4b8ec6baba12f34de40076c8e6c9
kitk4t3 <catalinadiaz30102009@gmail.com>
2025-08-18 16:05:48 +0000

    Actividad 1

--- MAIN.py
+++ MAIN.py
@@ -1,12 +1,14 @@
-# Historia 2
-# La serie sigue la historia de [Johnny] [Lawrence], exestudiante del dojo [Cobra Kai],
-# quien busca la redención reabriendo el infame dojo y reavivando, en el proceso,
-# su rivalidad con un ahora exitoso Daniel [LaRusso].
-# Este enemigo ha estado luchando por mantener el equilibrio en su vida sin la guía de su mentor, el señor [Miyagi].
-# La serie trata sobre dos hombres que enfrentan sus demonios del pasado y
-# resuelven las frustraciones del presente de la única manera en que saben hacerlo: a través del karate.
-=input
-=input
-=input
-=input
-=input
+import random
+dado=0
+contador=0
+puntaje=0
+while contador<10 and puntaje<38:
+  dado=random.randint(1,6)
+  print(dado)
+  puntaje=puntaje+dado
+  contador=contador+1
+print("El puntaje total es: " + str(puntaje))
+if puntaje >=38:
+  print("Ganaste")
+else:
+  print("Perdiste")
